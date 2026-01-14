0772c5298f3229cdcdcc0409e6cf66fa7c621aca
cartoonmakerhin-sudo <cartoonmakerhin@gmail.com>
2026-01-14 16:06:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -72,7 +72,7 @@ SUPPORT_CHAT = environ.get("SUPPORT_CHAT", "https://t.me/MoviesSearchPro")
 MOVIE_GROUP_LINK = environ.get("MOVIE_GROUP_LINK", "https://t.me/MoviesSearchPro")
 
 # Verification
-IS_VERIFY = is_enabled("IS_VERIFY", True)
+IS_VERIFY = is_enabled("IS_VERIFY", False)
 # ---------------------------------------------------------------
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/ProBotXUpdate")
 TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/ProBotXUpdate")
